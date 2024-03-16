bd9bed5245655ace480531b082c9602fe339842a
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-03-16 14:37:00 +0530

    Edureka course day-44 (express part-03, Friday, 15/03/2024)

---
+++ edureka-express-course/part-03/index.js
@@ -0,0 +1,30 @@
+/* 
+ architecture of nginx
+ master, worker, proxy cache, cache loader, cache manager
+*/
+
+/* 
+    CORS
+        same origin 
+        cross origin 
+            cross origin problem
+*/
+
+/* 
+ properties of domain and url
+    server type, host name, port, top-level domain, method/protocol 
+*/
+
+/* 
+    handle cross origin requrest
+        simple request
+        request with credentials
+        preflight request
+*/
+
+/* 
+    MVC Architecture
+        modal => It has data
+        view 
+        controller 
+*/
