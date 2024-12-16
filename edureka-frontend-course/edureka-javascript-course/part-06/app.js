8e86cf5118870c7c4168b05d12c4f264bc0c3594
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-12-16 09:20:41 +0530

    Edureka course day-45, class-32 (Javascript-06, Wednesday, 21/02/2024)

--- edureka-frontend-course/edureka-javascript-course/part-06/app.js
+++ edureka-frontend-course/edureka-javascript-course/part-06/app.js
@@ -1,38 +1,69 @@
-// for in loop
+// === check value and datatype
+console.log(1 === "1");
 
-const obj = {
-  name: "pranjal",
-  age: 24,
-  education: "b-tech",
-};
+// == check only value
+console.log(1 == "1");
 
-for (let key in obj) {
-  console.log(`${key} => ${obj[key]}`);
+// logical operators
+
+// and
+console.log(true && false);
+console.log(10 > 20 && 10 < 20);
+
+// or
+console.log(true || false);
+console.log(10 > 20 || 10 < 20);
+
+// not
+console.log(!false);
+console.log(!(10 > 20 || 10 < 20));
+
+// conditional statements
+
+// if statement
+let a = 10;
+let b = 20;
+
+if (a > b) {
+  // when condition is true then execute all the line of code inside the block
+  console.log("a is greater than b");
 }
 
-// for of loop
-
-const arr = ["honda", "bmw", "audi", "lambo"];
-
-for (let i of arr) {
-  console.log(i);
+// if else statement
+if (a > b) {
+  // when condition is true then execute all the line of code inside the block
+  console.log("a is greater than b");
+} else {
+  // when condition is false then execute all the line of code inside the block
+  console.log("a is less than b");
 }
 
-// for of and for in,in a string
-const str = "alphabates";
-for (let letter of str) {
-  console.log(letter);
-}
-for (let letter in str) {
-  console.log(str[letter]);
+// else if statement
+const age = 35;
+if (age > 50) {
+  // when condition is true then execute all the line of code inside the block
+  console.log("age is greater than 50");
+} else if (age > 30) {
+  // when condition is true then execute all the line of code inside the block
+  console.log("age is greater than 30");
+} else {
+  // when condition is false then execute all the line of code inside the block
+  console.log("age is less than 30");
 }
 
-// innerHTML
-const change = () => {
-  const element = document.getElementById("id");
-  element.innerHTML = "INTERNSHIP PROGRAM";
-};
-
-// prompt
-const name = prompt("enter your name");
-document.write(name);
+// switch statement
+const color = "green";
+switch (color) {
+  case "red":
+    console.log("color is red");
+    break;
+  case "blue":
+    console.log("color is blue");
+    break;
+  case "green":
+    console.log("color is green");
+    break;
+  default:
+    console.log("color is not red, blue or green");
+    break;
+}
