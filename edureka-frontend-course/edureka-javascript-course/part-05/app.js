6ac03eecdad0ca3204a19f37754d9405b6f922e9
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-12-16 08:35:17 +0530

    Edureka course day-44, class-31 (Javascript-05, Tuesday, 20/02/2024)

--- edureka-frontend-course/edureka-javascript-course/part-05/app.js
+++ edureka-frontend-course/edureka-javascript-course/part-05/app.js
@@ -1,3 +1,5 @@
+// Edureka course day-44, class-31 (Javascript-05, Tuesday, 20/02/2024)
+
 let a = 3;
 let b = 2;
 
@@ -9,8 +11,35 @@ console.log(a % b);
 console.log(a ** b);
 
 // increment and decrement
-a++;
-console.log(a);
+const c = a++; // post increment
+console.log(a); // a gets increment bcz of post increment
+const d = ++a; // pre increment
+console.log(c, d);
 
-b--;
-console.log(b);
+const e = b--; // post decrement
+console.log(b); // b gets decrement bcz of post decrement
+const f = --b; // pre decrement
+console.log(e, f);
+
+// assignment operators
+let apple = 10;
+let orange = 20;
+apple += 10; // apple = apple + 10
+apple -= 10; // apple = apple - 10
+apple *= 10; // apple = apple * 10
+apple /= 10; // apple = apple / 10
+apple %= 10; // apple = apple % 10
+
+apple = apple + orange;
+
+console.log(apple);
+
+// comparison operators
+console.log(10 > 20);
+console.log(10 < 20);
+console.log(10 >= 20);
+console.log(10 <= 20);
+console.log(10 == 20);
+console.log(10 === 20);
+console.log(10 != 20);
+console.log(10 !== 20);
