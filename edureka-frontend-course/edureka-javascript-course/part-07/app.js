84d8ee1b5e9f890770ee06dfff7883efa38f1471
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-12-29 10:17:00 +0530

    Edureka course day-46, class-33 (Javascript-07, Thursday, 22/02/2024)

---
+++ edureka-frontend-course/edureka-javascript-course/part-07/app.js
@@ -0,0 +1,24 @@
+// Edureka course day-46, class-33 (Javascript-07, Thursday, 22/02/2024)
+
+// for loop
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+console.log("**********");
+
+// while loop
+let i = 0;
+while (i < 10) {
+  console.log(i);
+  i++;
+}
+
+console.log("**********");
+
+// do while loop
+let j = 0;
+do {
+  console.log(j);
+  j++;
+} while (j < 10);
