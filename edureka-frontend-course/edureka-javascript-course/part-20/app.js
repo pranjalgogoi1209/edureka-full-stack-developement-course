db55522946528dfad9c64fae9e1a9ae4bc0a52b3
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2025-01-10 21:35:35 +0530

    Edureka course day-65, class-46 (Javascript-20, Tuesday, 12/03/2024)

---
+++ edureka-frontend-course/edureka-javascript-course/part-20/app.js
@@ -0,0 +1,7 @@
+// Edureka course day-65, class-46 (Javascript-20, Tuesday, 12/03/2024)
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  console.log("button is clicked");
+});
---
+++ edureka-frontend-course/edureka-javascript-course/part-21/app.js
@@ -0,0 +1,1 @@
+// Edureka course day-66, class-47 (Javascript-21, Wednesday, 13/03/2024)
